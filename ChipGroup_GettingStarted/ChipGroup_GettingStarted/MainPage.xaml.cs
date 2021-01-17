4cd73544efac56ecc994d22a8c3ab5913c54e7b9
Mohanlal Gopu <mohanlal.gopu@syncfusion.com>
2021-01-17 22:59:00 +0530

    1 . update new sample.

---
+++ ChipGroup_GettingStarted/ChipGroup_GettingStarted/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ChipGroup_GettingStarted
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var viewModel = (BindingContext as ViewModel);
+            viewModel.SelectedItems.Add(viewModel.Languages[2]);
+        }
+
+    }
+}
---
+++ ChipGroup_GettingStarted/ChipGroup_GettingStarted/ViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace ChipGroup_GettingStarted
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+
+        private ObservableCollection<Language> languages;
+        public ObservableCollection<Language> Languages
+        {
+            get
+            {
+                return languages;
+            }
+            set
+            {
+                languages = value;
+                OnPropertyChanged("Languages");
+            }
+        }
+
+        private ObservableCollection<Language> selectedItems;
+        public ObservableCollection<Language> SelectedItems
+        {
+            get
+            {
+                return selectedItems;
+            }
+            set
+            {
+                selectedItems = value;
+                OnPropertyChanged("SelectedItems");
+            }
+        }
+
+        public ViewModel()
+        {
+            Languages = new ObservableCollection<Language>();
+            Languages.Add(new Language() { Name = "C#" });
+            Languages.Add(new Language() { Name = "HTML" });
+            Languages.Add(new Language() { Name = "Java" });
+            Languages.Add(new Language() { Name = "JS" });
+
+            selectedItems = new ObservableCollection<Language>() { Languages[0], Languages[1] };
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
